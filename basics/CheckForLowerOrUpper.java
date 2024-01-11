edb604bd1f54f3e5983cf94d67922f12d819cc0c
Pushpa Leela Kumari <108827336+Neetable-PushpaLeelaKumari@users.noreply.github.com>
2024-01-11 17:54:50 +0530

    Basics

---
+++ basics/CheckForLowerOrUpper.java
@@ -0,0 +1,14 @@
+package basics;
+
+// Lower case characters 97 - 122
+// Upper case ascii values 65 - 90
+// 
+// 
+import java.util.Scanner;
+
+public class CheckForLowerOrUpper {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String ch = sc.next();
+    }
+}
---
+++ two_d_array/SumOfElements.java
@@ -0,0 +1,28 @@
+package two_d_array;
+
+import java.util.*;
+
+public class SumOfElements {
+    public static int SumAllElements(int arr[][]) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                sum = sum + arr[i][j];
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+        int arr[][] = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println(SumAllElements(arr));
+    }
+}
