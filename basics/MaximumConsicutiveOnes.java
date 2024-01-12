de7eea7c753ea5b3cbcfafc7dc742f4c479d74c8
Pushpa Leela Kumari <108827336+Neetable-PushpaLeelaKumari@users.noreply.github.com>
2024-01-12 15:11:41 +0530

    Find the Maximum Consicutive Ones in an array

---
+++ basics/MaximumConsicutiveOnes.java
@@ -0,0 +1,37 @@
+package basics;
+
+import java.util.*;
+
+public class MaximumConsicutiveOnes {
+    public static int FindMaxConsicutiveOnes(int arr[]) {
+        int max = 0;
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                count++;
+            } else {
+                if (max < count) {
+                    max = count;
+                }
+                count = 0;
+            }
+            if (i == arr.length - 1) {
+                if (max < count) {
+                    max = count;
+                }
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(FindMaxConsicutiveOnes(arr));
+    }
+
+}
