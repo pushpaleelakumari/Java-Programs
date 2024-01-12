8e5676883ab07c978c631da69018473fc0d89046
Pushpa Leela Kumari <108827336+Neetable-PushpaLeelaKumari@users.noreply.github.com>
2024-01-12 11:45:33 +0530

    Find missing element in an array

---
+++ basics/FindMissingElement.java
@@ -0,0 +1,34 @@
+package basics;
+
+import java.util.*;
+
+public class FindMissingElement {
+    public static int FindMissingNumber(int arr[]) {
+        int res = 1;
+        int temp = res;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[j] == res) {
+                    System.out.println("hello find");
+                    temp = res;
+                    res++;
+                }
+            }
+            if (temp == res) {
+                System.err.println("hello return");
+                return res;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(FindMissingNumber(arr));
+    }
+}
