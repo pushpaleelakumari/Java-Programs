0dd0a5a0da0ecbcad3ae378c87803047561e3e2f
Pushpa Leela Kumari <108827336+Neetable-PushpaLeelaKumari@users.noreply.github.com>
2024-01-15 11:22:22 +0530

    Day 3

--- TargetIndex.java
+++ TargetIndex.java
@@ -3,10 +3,15 @@ import java.util.*;
 public class TargetIndex {
 
     public static void PrintTargetIndexes(int a[], int target) {
+        int flag = 0;
         for (int i = 0; i < a.length; i++) {
             if (a[i] == target) {
                 System.out.print(i + " ");
+                flag = 1;
             }
+        }
+        if (flag == 0) {
+            System.out.println(-1);
         }
     }
 
